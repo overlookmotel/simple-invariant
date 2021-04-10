b83b313c827d32cf143a709953533ba9cd173bf4
overlookmotel <theoverlookmotel@gmail.com>
2021-04-10 18:17:45 +0100

    Omit invariant function itself from stack trace [fix]

--- index.js
+++ index.js
@@ -11,5 +11,9 @@ const DEFAULT_MESSAGE = 'Invariant failed';
 // Exports
 
 module.exports = function invariant(condition, message) {
-	if (!condition) throw new Error(message || DEFAULT_MESSAGE);
+	if (!condition) {
+		const err = new Error(message || DEFAULT_MESSAGE);
+		Error.captureStackTrace(err, invariant);
+		throw err;
+	}
 };
--- test/index.test.js
+++ test/index.test.js
@@ -25,6 +25,14 @@ describe('throws error if condition false', () => {
 	it('with empty error message', () => {
 		expect(() => invariant(false, '')).toThrow(new Error('Invariant failed'));
 	});
+
+	it('with stack trace not including invariant function itself', () => {
+		const err = tryCatch(() => invariant(false));
+		expect(err).toBeInstanceOf(Error);
+		const lines = err.stack.split(/\r?\n\s*/);
+		expect(lines[0]).toBe('Error: Invariant failed');
+		expect(lines[1]).toStartWith(`at fn (${__filename}:`);
+	});
 });
 
 describe('does not throw error if condition true', () => {
@@ -36,3 +44,12 @@ describe('does not throw error if condition true', () => {
 		expect(() => invariant(true)).not.toThrow();
 	});
 });
+
+function tryCatch(fn) {
+	try {
+		fn();
+		return undefined;
+	} catch (err) {
+		return err;
+	}
+}
